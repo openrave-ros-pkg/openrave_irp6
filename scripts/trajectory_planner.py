a0cc802db6b186ad15db2f69719721c7a8c8c286
Przemysław Krajewski <fokaszot@gmail.com>
2015-04-17 13:56:01 +0200

    loading urdf file collision free planner test

--- scripts/trajectory_planner.py
+++ scripts/trajectory_planner.py
@@ -5,6 +5,7 @@ import tf
 import actionlib
 
 from irpos import *
+from operator import *
 
 import irp6kinematic
 
@@ -30,6 +31,8 @@ if __name__ == '__main__':
 	module = RaveCreateModule(env, 'urdf')
 	name = module.SendCommand('load package://irp6_description/robots/irp6pboth.urdf.xacro package://irp6_description/robots/irp6pboth.srdf')
 	robot = env.GetRobot(name)
+	conveyor = env.ReadKinBodyXMLFile('data/conveyor.kinbody.xml')
+	env.Add(conveyor)
  
 	basemanip = interfaces.BaseManipulation(robot)
 	taskprob = interfaces.TaskManipulation(robot)
@@ -44,17 +47,20 @@ if __name__ == '__main__':
 	
 	time.sleep(5)
 
+	track.move_to_synchro_position(10.0)
+	postument.move_to_synchro_position(10.0)
 	
 	print "OpenRAVE move on!"
 	robot.SetActiveManipulator('postument');
+	robot.SetDOFValues(postument.get_joint_position(),postument_o.GetArmIndices())
 	with env:
-		sol = [-1.38227752354415, -1.541861095576026, 5.504115800705756e-05, 1.0007174886590251, 4.754815971689398, -1.91731301362624]
+		sol = [-1.28227752354415, -1.541861095576026, 5.504115800705756e-05, 1.0007174886590251, 4.754815971689398, -1.91731301362624]
 		traj=basemanip.MoveManipulator(sol,outputtrajobj=True,execute=False)
 	while not robot.GetController().IsDone():
 		time.sleep(0.01)	
 	robot.SetDOFValues(sol,postument_o.GetArmIndices())
 
-	postument.move_to_joint_position([-1.38227752354415, -1.541861095576026, 5.504115800705756e-05, 1.0007174886590251, 4.754815971689398, -1.91731301362624], 3.0)
+	postument.move_to_joint_position([-1.28227752354415, -1.541861095576026, 5.504115800705756e-05, 1.0007174886590251, 4.754815971689398, -1.91731301362624], 3.0)
 
 	"""print "IRPOS postument move on!"
 	goal = FollowJointTrajectoryGoal()
@@ -69,6 +75,8 @@ if __name__ == '__main__':
 
 	print "OpenRAVE move on!"
 	track_o=robot.SetActiveManipulator('track');
+	robot.SetDOFValues(track.get_joint_position(),track_o.GetArmIndices())
+	print "position: " + str(pos)
 	with env:
 		sol = irp6kinematic.solveIKTrack(env,[-0.000379723678393, -0.999880665371, 0.00120047894583, 0.0153970671608],[0.60904485399, 1.30059724452807, 1.20842285353])
 		traj=basemanip.MoveManipulator(sol,outputtrajobj=True,execute=False)
@@ -87,15 +95,19 @@ if __name__ == '__main__':
 		velGroup = None;
 	joints = []
 	vels = []
+	accs = []
 	for i in range (0,traj.GetNumWaypoints()):
 		w=traj.GetWaypoint(i);		
 		joints.append([w[jointGroup.offset],w[jointGroup.offset+1],w[jointGroup.offset+2],w[jointGroup.offset+3],w[jointGroup.offset+4],w[jointGroup.offset+5]]);
 		vels.append([w[velGroup.offset],w[velGroup.offset+1],w[velGroup.offset+2],w[velGroup.offset+3],w[velGroup.offset+4],w[velGroup.offset+5]]);
-		
-		
 	points = []
+	time=1;
 	for i in range(1,len(joints)):
-		points.append( JointTrajectoryPoint(joints[i], vels[i], [], [], rospy.Duration(i*5.0)) )
+		time = time+ (joints[i][0]-joints[i-1][0]) / (  vels[i-1][0]+  (vels[i][0]-vels[i-1][0])/2 )
+		if (joints[i][0]-joints[i-1][0]) / (  vels[i-1][0]+  (vels[i][0]-vels[i-1][0])/2 ) < 1.0:
+			continue
+		print str(time)
+		points.append( JointTrajectoryPoint(joints[i], vels[i], [], [], rospy.Duration(time)) )
 	track.move_along_joint_trajectory(points)
 	#track.move_to_joint_position([0.1, 0.0, -1.5418065817051163, 0.0, 1.5, 1.57, -1.57], 3.0)
 
@@ -107,6 +119,7 @@ if __name__ == '__main__':
 
 	print "OpenRAVE move on!"
 	track_o=robot.SetActiveManipulator('track');
+	robot.SetDOFValues(track.get_joint_position(),track_o.GetArmIndices())
 	with env:
 		sol = irp6kinematic.solveIKTrack(env,[-0.000379723678393, -0.999880665371, 0.00120047894583, 0.0153970671608],[0.00904485399, 1.30059724452807, 1.20842285353])
 		traj=basemanip.MoveManipulator(sol,outputtrajobj=True,execute=False)
@@ -125,15 +138,19 @@ if __name__ == '__main__':
 		velGroup = None;
 	joints = []
 	vels = []
+	accs = []
 	for i in range (0,traj.GetNumWaypoints()):
 		w=traj.GetWaypoint(i);		
 		joints.append([w[jointGroup.offset],w[jointGroup.offset+1],w[jointGroup.offset+2],w[jointGroup.offset+3],w[jointGroup.offset+4],w[jointGroup.offset+5]]);
 		vels.append([w[velGroup.offset],w[velGroup.offset+1],w[velGroup.offset+2],w[velGroup.offset+3],w[velGroup.offset+4],w[velGroup.offset+5]]);
-		
-		
 	points = []
+	time=1;
 	for i in range(1,len(joints)):
-		points.append( JointTrajectoryPoint(joints[i], vels[i], [], [], rospy.Duration(i*5.0)) )
+		time = time+ (joints[i][0]-joints[i-1][0]) / (  vels[i-1][0]+  (vels[i][0]-vels[i-1][0])/2 )
+		if (joints[i][0]-joints[i-1][0]) / (  vels[i-1][0]+  (vels[i][0]-vels[i-1][0])/2 ) < 1.0:
+			continue
+		print str(time)
+		points.append( JointTrajectoryPoint(joints[i], vels[i], [], [], rospy.Duration(time)) )
 	track.move_along_joint_trajectory(points)
 
 
